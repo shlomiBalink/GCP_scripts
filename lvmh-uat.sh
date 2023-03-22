9474d0201f6329e923e4b7cc32cbc0c8f7fb4f35
shlomiBalink <shlomib@balink.net>
2023-03-22 13:08:27 +0200

    adding environments

--- lvmh-uat.sh
+++ lvmh-uat.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
 ### configuration
-export PROJECT_ID=ken-em-prj-apps-cltg-01-dev
-export CLUSTER_NAME=ken-em-kub-cltg-01-dev
+export PROJECT_ID=ken-em-prj-clt-01-ppd
+export CLUSTER_NAME=ken-em-kub-clt-01-ppd
 export CLUSTER_LOCATION=europe-west1-b
-export BASTION_NAME=kenembas01d
+export BASTION_NAME=kenembas01s
 export BASTION_LOCATION=europe-west1-b
 # This port will be opened on your laptop and it should be uniq if you want to connect to several cluster(s)
 export STATIC_PORT=8888
 # We are defining a more explicit context name
-export CONTEXT_NAME=kenzo-dev
+export CONTEXT_NAME=kenzo-ppd
 export GCP_NAMESAPCE=clienteling-backend
 export ELASTIC_POD=es-product-catalog-es-default-0
 ### main script
